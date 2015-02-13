633386f7c62e801c254918278d1e4f7d5ed84192
Thomas Merkel <tm@core.io>
2015-02-13 13:50:24 +0100

    Add script that read out sdc:nic ip information to generate nsd_listen.conf file
    If we could read the information from sdc:nic and parse it we listen on
    that ip addresses. If not the nsd_listen.conf is cleaned to we try to
    listen on all interface (which moslty result in the bug).
    
    We're also sure we listen on localhost ipv4 and ipv6.

--- copy/var/zoneinit/includes/30-nsd.sh
+++ copy/var/zoneinit/includes/30-nsd.sh
@@ -5,5 +5,17 @@
 sudo -u nsd HOME=/opt/local/etc/nsd nsd-control-setup
 rm /opt/local/etc/nsd/.rnd
 
+# Create nsd_listen.conf file with all ip addresses managed
+# by vmadm sdc:nics / mdata
+NSD_LISTEN_CONF='/opt/local/etc/nsd/nsd_listen.conf'
+echo > ${NSD_LISTEN_CONF}
+if mdata-get sdc:nics 1>/dev/null 2>&1; then
+	echo "ip-address: 127.0.0.1" >  ${NSD_LISTEN_CONF}
+	echo "ip-address: ::1"       >> ${NSD_LISTEN_CONF}
+	for ip in $(mdata-get sdc:nics | json -a ip); do
+		echo "ip-address: ${ip%/*}" >> ${NSD_LISTEN_CONF}
+	done
+fi
+
 # Enable nsd service
 svcadm enable svc:/network/nsd:default
