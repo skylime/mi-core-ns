f9210b20364cedbfecd7fdae84032a6244ac9d10
Thomas Merkel <tm@core.io>
2019-10-29 11:54:36 +0100

    Update cronjob to download netmgt information every 5 minutes

--- copy/var/zoneinit/includes/31-core-ns-download.sh
+++ copy/var/zoneinit/includes/31-core-ns-download.sh
@@ -1,4 +1,4 @@
 # Setup cron for downloading ns information
 
-CRON='0,15,30,45 * * * * /opt/core/bin/core-ns-download.py -u "$(mdata-get ns_urls 2>/dev/null)"'
+CRON='0,5,10,15,20,25,30,35,40,45,50,55 * * * * /opt/core/bin/core-ns-download.py -u "$(mdata-get ns_urls 2>/dev/null)"'
 (crontab -l 2>/dev/null || true; echo "$CRON" ) | sort | uniq | crontab
